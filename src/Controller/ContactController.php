fab1cb1617c074e7892b95f5adae74bb2737c149
logan <loloherbo.drekoh@gmail.com>
2024-04-26 18:23:30 +0200

    26.04-mailService

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,47 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
+
 use App\Service\MailService;
 use App\Form\ContactFormType;
-use Symfony\Component\Mime\Email;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class ContactController extends AbstractController
 {
+
     #[Route('/contact', name: 'app_contact')]
-    #[IsGranted("ROLE_CLIENT")]
-    public function contact(Request $request, EntityManagerInterface $em, MailService $ms): Response
+    public function mail(Request $request, MailService $ms)
     {
-        $data_mail = new Contact();
-        $form = $this->createForm(ContactFormType::class, $data_mail);
-        $form->handleRequest($request);
-
-        $data_mail = new Contact();
-
-        $form = $this->createForm(ContactFormType::class, $data_mail);
+        $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $em->persist($data_mail);
-            $em->flush();
-
-            $email = (new TemplatedEmail())
-                ->from($data_mail->getEmail())
-                ->to('The_District@gmail.com')
-                ->subject('Demande de contact')
-                ->htmlTemplate('contact/mail.html.twig')
-                ->context(['data_mail' => $data_mail]);
-
-            $ms->sendEmail($email);
+            $data = $form->getData();
+            $ms->sendEmail($data);
+            $ms->sendEmailConfirmation($data);
             return $this->redirectToRoute('app_accueil');
         }
 
@@ -50,67 +30,4 @@ class ContactController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-    // #[Route('/contact', name: 'app_contact')]
-    // #[IsGranted("ROLE_CLIENT")]
-    // public function contact(Request $request, MailerInterface $mailer, EntityManagerInterface $em): Response
-    // {
-    //     $data_mail = new Contact();
-    //     $form = $this->createForm(ContactFormType::class, $data_mail);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-
-    //         $em->persist($data_mail);
-    //         $em->flush();
-
-
-    //         $email = (new TemplatedEmail())
-    //             ->from($data_mail->getEmail())
-    //             ->to('The_District@gmail.com')
-    //             ->subject('Demande de contact')
-    //             ->htmlTemplate('contact/mail.html.twig')
-    //             ->context(['data_mail' => $data_mail]);
-
-
-
-    //         $mailer->send($email);
-
-
-    //         $emailConfirmation = (new Email())
-    //             ->from('The_District@gmail.com')
-    //             ->to($data_mail->getEmail())
-    //             ->subject('Confirmation de votre demande de contact')
-    //             ->text('Merci pour votre demande de contact. Nous avons bien reçu vos informations.');
-
-    //         $mailer->send($emailConfirmation);
-
-    //         return $th        // $this->mailService->sendEmail($request);
-    // return $this->redirectToRoute('app_accueil');is->redirectToRoute('app_accueil');
-    //     }
-
-    //     return $this->render('contact/index.html.twig', [
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
-
-
-    // private $mailService;
-
-    // public function __construct(MailService $mailService)
-    // {
-    //     $this->mailService = $mailService;
-    // }
-
-    // #[Route('/contact', name: 'app_contact')]
-    // public function mail(Request $request): Response
-    // {
-    //     $data_mail = new Contact();
-    //     $form = $this->createForm(ContactFormType::class, $data_mail);
-    //     $form->handleRequest($request);
-    //     return $this->render('contact/index.html.twig', [
-    //         'form' => $form->createView(),
-    //     ]);$from, $to, $subject, $message
-    //     // $this->mailService->sendEmail($request);
-    //     // return $this->redirectToRoute('app_accueil');
-    // }
 }
--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -3,26 +3,41 @@
 namespace App\Service;
 
 use App\Entity\Contact;
-use App\Form\ContactFormType;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Form\FormFactoryInterface;
 
 class MailService
 {
     private $mailer;
-    private $formFactory;
-    private $entityManager;
 
     public function __construct(MailerInterface $mailer)
     {
         $this->mailer = $mailer;
     }
 
-    public function sendEmail($message): void
+    public function sendEmail(Contact $data): void
     {
-        $this->mailer->send($message);
+        $email = (new TemplatedEmail())
+            ->from($data->getEmail())
+            ->to('The_District@gmail.com')
+            ->subject('Demande de contact')
+            ->htmlTemplate('contact/mail.html.twig')
+            ->context(['data_mail' => $data]);
+
+        $this->mailer->send($email);
+    }
+
+    public function sendEmailConfirmation(Contact $data): void
+    {
+
+
+        $emailConfirmation = (new Email())
+            ->from('The_District@gmail.com')
+            ->to($data->getEmail())
+            ->subject('Confirmation de votre demande de contact')
+            ->text('Merci pour votre demande de contact. Nous avons bien reçu vos informations.');
+
+        $this->mailer->send($emailConfirmation);
     }
 }
